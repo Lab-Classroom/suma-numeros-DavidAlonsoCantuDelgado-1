cfe9f64082b56ea135a7e3c6b4c3c79150a57aa2
David Cantú Delgado <42122802+DavidCantuDelgado@users.noreply.github.com>
2021-06-07 15:42:12 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 def main():
     #Prueba 2
     suma = 0
-    num = int(input("Teclea un número: "))
+    num = int(input("Teclea un número:"))
     while num != 0:
         suma = suma + num
-        num = int(input("Teclea un número: "))
+        num = int(input("Teclea un número:"))
 
     print("La suma de todos los números es: {}".format(suma))
 
